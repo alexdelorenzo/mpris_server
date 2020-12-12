559a93fab0ab0bcc07689cf04d451ddefae590c1
alexdelorenzo <you@example.com>
2020-12-12 18:11:05 -0500

    Increment version for release

--- setup.py
+++ setup.py
@@ -1,23 +1,32 @@
 from setuptools import setup
 from pathlib import Path
 
-requirements = \
+
+REQS = \
   Path('requirements.txt') \
     .read_text() \
     .split('\n')
-readme = Path('README.md').read_text()
+
+REQS = [
+  req
+  for req in REQS
+  if not req.strip().startswith('#')
+]
+
+README = Path('README.md').read_text()
+
 
 setup(
   name="mpris_server",
-  version="0.2.14",
-  description="Publish a MediaPlayer2 MPRIS device to D-BUS.",
-  long_description=readme,
+  version="0.2.16",
+  description="Publish a MediaPlayer2 MPRIS device to D-Bus.",
+  long_description=README,
   long_description_content_type="text/markdown",
   url="https://alexdelorenzo.dev",
   author="Alex DeLorenzo",
   license="AGPL-3.0",
   packages=['mpris_server'],
   zip_safe=True,
-  install_requires=requirements,
+  install_requires=REQS,
   python_requires='>=3.6',
 )
