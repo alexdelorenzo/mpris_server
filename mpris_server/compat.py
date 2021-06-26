f11bc5cd3f878abb47b3f49bdb02ef7ecb046596
alexdelorenzo <alex@alexdelorenzo.dev>
2021-06-26 19:17:49 -0400

    Refactor

--- mpris_server/compat.py
+++ mpris_server/compat.py
@@ -16,11 +16,11 @@ from .metadata import Metadata, DbusMetadata, DbusTypes, \
 
 logger = logging.getLogger(__name__)
 
-DBUS_NAME_MAX = 255
-START_WITH = "_"
-FIRST_CHAR = 0
+DBUS_NAME_MAX: int = 255
+START_WITH: str = "_"
+FIRST_CHAR: int = 0
 
-# following must be subscriptable to be used with choices()
+# following must be subscriptable to be used with random.choices()
 VALID_CHARS_SUB: tuple[str] = tuple(VALID_CHARS)
 INTERFACE_CHARS: set[str] = {*VALID_CHARS, '-'}
 
@@ -60,11 +60,7 @@ def get_dbus_name(
     return get_dbus_name(random_name())
 
   # interface names can contain hyphens
-  if is_interface:
-    valid_chars = {*VALID_CHARS, '-'}
-
-  else:
-    valid_chars = VALID_CHARS
+  valid_chars = INTERFACE_CHARS if is_interface else VALID_CHARS
 
   # convert utf8 to ascii
   new_name = to_ascii(name)
@@ -72,19 +68,19 @@ def get_dbus_name(
   # new name shouldn't have spaces
   new_name = new_name.replace(' ', '_')
 
-  # new name should only contain DBus valid chars
+  # new name should only contain D-Bus valid chars
   new_name = ''.join(
     char
     for char in new_name
     if char in valid_chars
   )
 
-  # DBus names can't start with numbers
+  # D-Bus names can't start with numbers
   if new_name and new_name[FIRST_CHAR].isnumeric():
     # just stick an underscore in front of the number
     return START_WITH + new_name
 
-  # but they can start with letters or underscore
+  # but they can start with letters or underscores
   elif new_name:
     return new_name
 
