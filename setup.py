51c6832a016059c90b3986de19782fdc17384e37
alexdelorenzo <alex@alexdelorenzo.dev>
2021-06-28 18:08:22 -0400

    Add submodules to packages

--- setup.py
+++ setup.py
@@ -5,6 +5,12 @@ from pathlib import Path
 #from mpris_server import __version__
 
 
+PKGS: List[str] = [
+  'mpris_server',
+  'mpris_server.mpris',
+  'mpris_server.interfaces'
+]
+
 REQS: List[str] = Path('requirements.txt') \
   .read_text() \
   .splitlines()
@@ -20,14 +26,14 @@ README: str = Path('README.md').read_text()
 
 setup(
   name="mpris_server",
-  version='0.4.0',
+  version='0.4.1',
   description="Publish a MediaPlayer2 MPRIS device to D-Bus.",
   long_description=README,
   long_description_content_type="text/markdown",
   url="https://alexdelorenzo.dev",
   author="Alex DeLorenzo",
   license="AGPL-3.0",
-  packages=['mpris_server'],
+  packages=PKGS,
   zip_safe=True,
   install_requires=REQS,
   python_requires='>=3.6',
