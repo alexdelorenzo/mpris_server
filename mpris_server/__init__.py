84005d7784df387ca507e8e915a0f5455dff39f5
alexdelorenzo <alex@alexdelorenzo.dev>
2021-06-30 17:40:06 -0400

    Add new annotations, update logging

--- mpris_server/__init__.py
+++ mpris_server/__init__.py
@@ -6,4 +6,4 @@ from .mpris.compat import get_dbus_name, enforce_dbus_length
 from .mpris.metadata import Metadata, MetadataObj, ValidMetadata
 
 
-__version__: str = '0.4.0'
+__version__: types.Final[str] = '0.4.1'
