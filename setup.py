77d85a9c2d176b8e627a585f85dd7307f6177f59
alexdelorenzo <alex@alexdelorenzo.dev>
2021-09-04 18:21:50 -0400

    Increment version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ README: str = Path('README.md').read_text()
 
 setup(
   name="mpris_server",
-  version='0.5.0',
+  version='0.5.1',
   description="⏯️ Publish a MediaPlayer2 MPRIS device to D-Bus.",
   long_description=README,
   long_description_content_type="text/markdown",
