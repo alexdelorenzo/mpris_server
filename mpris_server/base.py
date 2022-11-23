aae314466158b3af15886ec06b228a8c453c429f
Alex DeLorenzo <alex@alexdelorenzo.dev>
2022-11-23 00:14:36 -0500

    Introduce D-Bus property enum, add playback properties to player properties list, use rich type aliases

--- mpris_server/base.py
+++ mpris_server/base.py
@@ -4,7 +4,7 @@ from decimal import Decimal
 from enum import Enum, auto
 from os import PathLike
 from string import ascii_letters, digits
-from typing import Iterable, NamedTuple, Optional, TYPE_CHECKING, Union
+from typing import Iterable, NamedTuple, Optional, Self, TYPE_CHECKING, Union
 
 from gi.repository.GLib import Variant
 from strenum import StrEnum
@@ -16,9 +16,38 @@ if TYPE_CHECKING:
   from .interfaces.interface import MprisInterface
 
 
-Prop = str
-Props = list[Prop]
+class Property(StrEnum):
+  ActivePlaylist: Self = auto()
+  CanControl: Self = auto()
+  CanGoNext: Self = auto()
+  CanGoPrevious: Self = auto()
+  CanPause: Self = auto()
+  CanPlay: Self = auto()
+  CanQuit: Self = auto()
+  CanRaise: Self = auto()
+  CanSeek: Self = auto()
+  CanSetFullscreen: Self = auto()
+  DesktopEntry: Self = auto()
+  Fullscreen: Self = auto()
+  HasTrackList: Self = auto()
+  Identity: Self = auto()
+  LoopStatus: Self = auto()
+  MaximumRate: Self = auto()
+  Metadata: Self = auto()
+  MinimumRate: Self = auto()
+  Orderings: Self = auto()
+  PlaybackStatus: Self = auto()
+  PlaylistCount: Self = auto()
+  Position: Self = auto()
+  Rate: Self = auto()
+  Shuffle: Self = auto()
+  SupportedMimeTypes: Self = auto()
+  SupportedUriSchemes: Self = auto()
+  Tracks: Self = auto()
+  Volume: Self = auto()
 
+
+Properties = list[Property]
 
 INTERFACE: Final[str] = "org.mpris.MediaPlayer2"
 ROOT_INTERFACE: Final[str] = INTERFACE
@@ -27,43 +56,80 @@ NAME: Final[str] = "mprisServer"
 
 MIME_TYPES: Final[list[str]] = ["audio/mpeg", "application/ogg", "video/mpeg"]
 BUS_TYPE: Final[str] = "session"
-URI: Final[Props] = ["file"]
+URI: Final[list[str]] = ["file"]
 DEFAULT_DESKTOP: Final[str] = ''
 
 # typically, these are the props that D-Bus needs to be notified about
 # upon specific state-change events.
-ON_ENDED_PROPS: Final[Props] = ['PlaybackStatus']
-ON_VOLUME_PROPS: Final[Props] = ['Volume', 'Metadata']
-ON_PLAYBACK_PROPS: Final[Props] = [
-  'PlaybackStatus', 'Metadata', 'CanControl', 'Rate',
-  'MinimumRate', 'MaximumRate',
+ON_ENDED_PROPS: Final[Properties] = [
+  Property.PlaybackStatus,
 ]
-ON_PLAYPAUSE_PROPS: Final[Props] = ['PlaybackStatus']
-ON_TITLE_PROPS: Final[Props] = ['Metadata']
-ON_OPTION_PROPS: Final[Props] = [
-  'LoopStatus', 'Shuffle', 'CanGoPrevious', 'CanGoNext',
-  'CanPlay', 'CanPause',
+ON_VOLUME_PROPS: Final[Properties] = [
+  Property.Volume,
+  Property.Metadata,
 ]
-ON_SEEK_PROPS: Final[Props] = ['Position', 'CanSeek']
+ON_PLAYBACK_PROPS: Final[Properties] = [
+  Property.PlaybackStatus,
+  Property.Metadata,
+  Property.CanControl,
+  Property.Rate,
+  Property.MinimumRate,
+  Property.MaximumRate,
+]
+ON_PLAYPAUSE_PROPS: Final[Properties] = [
+  Property.PlaybackStatus,
+]
+ON_TITLE_PROPS: Final[Properties] = [
+  Property.Metadata,
+]
+ON_OPTION_PROPS: Final[Properties] = [
+  Property.LoopStatus,
+  Property.Shuffle,
+  Property.CanGoPrevious,
+  Property.CanGoNext,
+  Property.CanPlay,
+  Property.CanPause,
+]
+ON_SEEK_PROPS: Final[Properties] = [
+  Property.Position,
+  Property.CanSeek
+]
 
 # all props for each interface
-ON_PLAYER_PROPS: Final[Props] = list({
-  *ON_ENDED_PROPS, *ON_VOLUME_PROPS, *ON_PLAYPAUSE_PROPS,
-  *ON_TITLE_PROPS, *ON_OPTION_PROPS, *ON_SEEK_PROPS,
+ON_PLAYER_PROPS: Final[Properties] = list({
+  *ON_ENDED_PROPS,
+  *ON_VOLUME_PROPS,
+  *ON_PLAYPAUSE_PROPS,
+  *ON_TITLE_PROPS,
+  *ON_OPTION_PROPS,
+  *ON_SEEK_PROPS,
+  *ON_PLAYBACK_PROPS,
 })
-ON_TRACKS_PROPS: Final[Props] = ['Tracks']
-ON_PLAYLIST_PROPS: Final[Props] = ['PlaylistCount', 'Orderings', 'ActivePlaylist']
-ON_ROOT_PROPS: Final[Props] = [
-  'CanQuit', 'Fullscreen', 'CanSetFullscreen', 'CanRaise',
-  'HasTrackList', 'Identity', 'DesktopEntry', 'SupportedUriSchemes',
-  'SupportedMimeTypes'
+ON_TRACKS_PROPS: Final[Properties] = [
+  Property.Tracks
+]
+ON_PLAYLIST_PROPS: Final[Properties] = [
+  Property.PlaylistCount,
+  Property.Orderings,
+  Property.ActivePlaylist
+]
+ON_ROOT_PROPS: Final[Properties] = [
+  Property.CanQuit,
+  Property.Fullscreen,
+  Property.CanSetFullscreen,
+  Property.CanRaise,
+  Property.HasTrackList,
+  Property.Identity,
+  Property.DesktopEntry,
+  Property.SupportedUriSchemes,
+  Property.SupportedMimeTypes,
 ]
 
 BEGINNING: Final[int] = 0
 
 DEFAULT_TRACK_ID: Final[str] = '/default/1'
 DEFAULT_PLAYLIST_COUNT: Final[int] = 1
-DEFAULT_ORDERINGS: Final[Props] = ["Alphabetical", "User"]
+DEFAULT_ORDERINGS: Final[list[str]] = ["Alphabetical", "User"]
 
 # valid characters for a DBus name
 VALID_PUNC: Final[str] = '_'
@@ -72,13 +138,13 @@ VALID_CHARS: Final[set[str]] = {*ascii_letters, *digits, *VALID_PUNC}
 NAME_PREFIX: Final[str] = "Mpris_Server_"
 RAND_CHARS: Final[int] = 5
 
-
 # type aliases
 Paths = Union[PathLike, str]
 
 # units and convenience aliases
 Microseconds = int
 Position = Microseconds
+Duration = Microseconds
 UnitInterval = Decimal
 Volume = UnitInterval
 Rate = UnitInterval
@@ -93,12 +159,10 @@ ActivePlaylist = tuple[PlaylistValidity, PlaylistEntry]
 # python, d-bus and mpris types
 PyType = Union[type, GenericAlias, _GenericAlias]
 DbusPyTypes = Union[str, float, int, bool, list]
-Attr = str
-AttrVals = dict[Attr, DbusPyTypes]
-DbusMetadata = dict[Attr, Variant]
+PropVals = dict[Property, DbusPyTypes]
+DbusMetadata = dict[Property, Variant]
 DbusType = str
 DbusObj = str
-
 
 DEFAULT_RATE: Final[Rate] = Rate(1.0)
 PAUSE_RATE: Final[Rate] = Rate(0.0)
@@ -158,7 +222,7 @@ class Track(NamedTuple):
   track_id: DbusObj = DEFAULT_TRACK_ID
   name: str = "Default Track"
   track_no: Optional[int] = None
-  length: Microseconds = 0
+  length: Duration = 0
   uri: Optional[str] = None
   artists: tuple[Artist] = tuple()
   album: Optional[Album] = None
@@ -169,15 +233,15 @@ class Track(NamedTuple):
 
 def dbus_emit_changes(
   interface: MprisInterface,
-  changes: Iterable[str]
+  changes: Iterable[Property]
 ):
-  attr_vals: AttrVals = {
-    attr: getattr(interface, attr)
-    for attr in changes
+  prop_vals: PropVals = {
+    prop: getattr(interface, prop)
+    for prop in changes
   }
 
   interface.PropertiesChanged(
     interface.INTERFACE,
-    attr_vals,
+    prop_vals,
     []
   )
