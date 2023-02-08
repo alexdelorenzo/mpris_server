d8da76f7d87c62c906ef35256d0b29740d309570
alexdelorenzo <alex@alexdelorenzo.dev>
2023-02-08 06:59:49 -0500

    Refactor exported API

--- mpris_server/__init__.py
+++ mpris_server/__init__.py
@@ -1,18 +1,23 @@
 from __future__ import annotations
 
+# modules
 from . import adapters, base, interfaces, mpris, server, types
-from .adapters import (
-  Metadata, MprisAdapter, MprisAdapter, Paths, PlayState, PlayState, PlayerAdapter, Rate, RootAdapter, Volume,
-)
-from .base import (
-  BEGINNING, DEFAULT_RATE, DbusObj, DbusObj, MIME_TYPES, Microseconds, Rate, Track, URI, Volume,
-)
-from .events import EventAdapter
-from .mpris import metadata
-from .server import Server
-
 from .interfaces import interface, player, playlists, root, tracklist
 
+# constants and aliases
+from .base import (
+  BEGINNING, DEFAULT_RATE, MIME_TYPES, URI, DbusObj, Microseconds, Paths, PlayState,
+  Rate, Track, Volume,
+)
+
+# classes
+from .adapters import (
+  MprisAdapter, PlayerAdapter, PlaylistAdapter, RootAdapter, TrackListAdapter,
+)
+from .events import EventAdapter
+from .server import Server
+
+# mpris spec
 from .mpris.compat import enforce_dbus_length, get_dbus_name, get_track_id
 from .mpris.metadata import Metadata, MetadataObj, ValidMetadata
 
