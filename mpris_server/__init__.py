b8082958ada0edd624b60ef6e259c71660eb4c4e
alexdelorenzo <alex@alexdelorenzo.dev>
2023-02-08 06:10:25 -0500

    Export user facing code in top module

--- mpris_server/__init__.py
+++ mpris_server/__init__.py
@@ -1,11 +1,20 @@
 from __future__ import annotations
 
-from . import adapters, base, types, server, mpris, interfaces
-from .interfaces import interface, player, root, playlists, tracklist
+from . import adapters, base, interfaces, mpris, server, types
+from .adapters import (
+  Metadata, MprisAdapter, MprisAdapter, Paths, PlayState, PlayState, PlayerAdapter, Rate, RootAdapter, Volume,
+)
+from .base import (
+  BEGINNING, DEFAULT_RATE, DbusObj, DbusObj, MIME_TYPES, Microseconds, Rate, Track, URI, Volume,
+)
+from .events import EventAdapter
 from .mpris import metadata
+from .server import Server
 
-from .mpris.compat import get_dbus_name, enforce_dbus_length
+from .interfaces import interface, player, playlists, root, tracklist
+
+from .mpris.compat import enforce_dbus_length, get_dbus_name, get_track_id
 from .mpris.metadata import Metadata, MetadataObj, ValidMetadata
 
 
-__version__: types.Final[str] = '0.6.0'
+__version__: types.Final[str] = "0.6.0"
