98fb53b486692248ac06164b16f5c8ac19beeee1
alexdelorenzo <3579286+alexdelorenzo@users.noreply.github.com>
2023-10-12 18:49:40 -0400

    Export interfaces

--- mpris_server/interfaces/__init__.py
+++ mpris_server/interfaces/__init__.py
@@ -1,1 +1,14 @@
- 
+from .interface import MprisInterface
+from .player import Player
+from .playlists import Playlists
+from .root import Root
+from .tracklist import TrackList
+
+
+__all__ = [
+  'MprisInterface',
+  'Player',
+  'Playlists',
+  'Root',
+  'TrackList',
+]
