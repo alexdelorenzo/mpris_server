55630c6cf410937040660926c8ecc38e99163df3
alexdelorenzo <3579286+alexdelorenzo@users.noreply.github.com>
2023-10-12 21:55:11 -0400

    Use stdlib annotations, sort

--- mpris_server/__init__.py
+++ mpris_server/__init__.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import Final
+
 from . import adapters, base, types, server, mpris, interfaces
 
 from .adapters import *
@@ -7,11 +9,11 @@ from .base import *
 from .enums import *
 from .events import *
 from .interfaces import *
+from .mpris import *
 from .server import *
 
-from .mpris import *
 from .mpris.compat import get_dbus_name, enforce_dbus_length, get_track_id
 from .mpris.metadata import Metadata, MetadataObj, ValidMetadata
 
 
-__version__: types.Final[str] = '0.8.0'
+__version__: Final[str] = '0.8.0'
