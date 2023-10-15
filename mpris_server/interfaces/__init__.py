00731a112b71ab83267c278e0b97d0609a950850
alexdelorenzo <3579286+alexdelorenzo@users.noreply.github.com>
2023-10-15 00:09:55 -0400

    Enumerate submodule public exports

--- mpris_server/interfaces/__init__.py
+++ mpris_server/interfaces/__init__.py
@@ -1,21 +1,22 @@
 from .interface import MprisInterface
 from .player import Player
 from .playlists import Playlists
-from .root import Root
+from .root import Root, get_desktop_entry
 from .tracklist import TrackList
 
 from . import interface, player, root, playlists, tracklist
 
 
 __all__ = [
+  'get_desktop_entry',
+  'interface',
   'MprisInterface',
   'Player',
-  'Playlists',
-  'Root',
-  'TrackList',
-  'interface',
   'player',
-  'root',
+  'Playlists',
   'playlists',
+  'Root',
+  'root',
+  'TrackList',
   'tracklist',
 ]
--- mpris_server/mpris/__init__.py
+++ mpris_server/mpris/__init__.py
@@ -1,7 +1,33 @@
 from . import compat, metadata
 
+from .compat import enforce_dbus_length, get_dbus_name, get_track_id, DBUS_NAME_MAX
+from .metadata import (
+  DEFAULT_METADATA, Name, Metadata, MetadataEntry, NameMetadata, SortedMetadata,
+  MetadataEntries, MetadataTypes, MetadataObj, ValidMetadata, get_runtime_types,
+  is_dbus_type, is_valid_metadata, get_dbus_metadata
+)
+
 
 __all__ = [
   'compat',
+  'DBUS_NAME_MAX',
+  'DEFAULT_METADATA',
+  'enforce_dbus_length',
+  'get_dbus_metadata',
+  'get_dbus_name',
+  'get_runtime_types',
+  'get_track_id',
+  'is_dbus_type',
+  'is_valid_metadata',
   'metadata',
+  'Metadata',
+  'MetadataEntries',
+  'MetadataEntry',
+  'MetadataObj',
+  'MetadataTypes',
+  'Name',
+  'NameMetadata',
+  'SortedMetadata',
+  'ValidMetadata',
 ]
+
