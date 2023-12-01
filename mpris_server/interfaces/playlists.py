8191b4ff0a788b7598ab50bcbcd064892d6939e0
alexdelorenzo <3579286+alexdelorenzo@users.noreply.github.com>
2023-12-01 17:48:12 -0500

    Remove unused code

--- mpris_server/interfaces/playlists.py
+++ mpris_server/interfaces/playlists.py
@@ -47,10 +47,6 @@ class Playlists(MprisInterface):
   @log_trace
   def ActivePlaylist(self) -> ActivePlaylist:
     return self.adapter.get_active_playlist()
-    # self.log_trace("Getting %s.ActivePlaylist", self.INTERFACE)
-    # playlist_is_valid = False
-    # playlist = ("/", "None", "")
-    # return (playlist_is_valid, playlist)
 
   @property
   @log_trace
@@ -65,51 +61,7 @@ class Playlists(MprisInterface):
   @log_trace
   def ActivatePlaylist(self, playlist_id: PlaylistId):
     self.adapter.activate_playlist(playlist_id)
-    # log.debug(
-    #     "%s.ActivatePlaylist(%r) called", self.INTERFACE, playlist_id
-    # )
-    # playlist_uri = get_playlist_uri(playlist_id)
-    # playlist = self.core.playlists.lookup(playlist_uri).get()
-    # if playlist and playlist.tracks:
-    #     tl_tracks = self.core.tracklist.add(playlist.tracks).get()
-    #     self.core.playback.play(tlid=tl_tracks[0].tlid).get()
 
   @log_trace
   def GetPlaylists(self, index: int, max_count: int, order: str, reverse: bool) -> list[PlaylistEntry]:
     return self.adapter.get_playlists(index, max_count, order, reverse)
-    # log.debug(
-    #     "%s.GetPlaylists(%r, %r, %r, %r) called",
-    #     self.INTERFACE,
-    #     index,
-    #     max_count,
-    #     order,
-    #     reverse,
-    # )
-    # playlists = self.core.playlists.as_list().get()
-    # if order == "Alphabetical":
-    #     playlists.sort(key=lambda p: p.name, reverse=reverse)
-    # elif order == "User" and reverse:
-    #     playlists.reverse()
-    # slice_end = index + max_count
-    # playlists = playlists[index:slice_end]
-    # results = [(get_playlist_id(p.uri), p.name, "") for p in playlists]
-    # return results
-
-
-#
-# def get_playlist_id(playlist_uri: Union[str, bytes]) -> str:
-#     # Only A-Za-z0-9_ is allowed, which is 63 chars, so we can't use
-#     # base64. Luckily, D-Bus does not limit the length of object paths.
-#     # Since base32 pads trailing bytes with "=" chars, we need to replace
-#     # them with an allowed character such as "_".
-#     if isinstance(playlist_uri, str):
-#         playlist_uri = playlist_uri.encode()
-#     encoded_uri = base64.b32encode(playlist_uri).decode().replace("=", "_")
-#     return "/com/mopidy/playlist/%s" % encoded_uri
-#
-#
-# def get_playlist_uri(playlist_id: Union[str, bytes]) -> str:
-#     if isinstance(playlist_id, bytes):
-#         playlist_id = playlist_id.decode()
-#     encoded_uri = playlist_id.split("/")[-1].replace("_", "=").encode()
-#     return base64.b32decode(encoded_uri).decode()
